aab6d8ac9efb48e86eaf086215835ec88f3d08c5
GedMarc <ged_marc@hotmail.com>
2020-10-12 00:17:44 +0200

    JDK 8 Open API and Swagger updates, CXF 3.4.0 services updates

--- OpenAPIExplorer/src/main/java/module-info.java
+++ OpenAPIExplorer/src/main/java/module-info.java
@@ -1,9 +1,14 @@
-module  {
+module com.jwebmp.apps.openapiexplorer {
+	requires com.jwebmp.core;
 
-	requires com.guicedee.guicedservlets.rest;
+	requires com.jwebmp.plugins.bootstrap4;
+	requires com.jwebmp.plugins.jqueryui;
+	requires com.jwebmp.plugins.fontawesome5;
+
 	requires com.guicedee.guicedservlets.undertow;
 
 	requires static lombok;
 
 }
+
 
